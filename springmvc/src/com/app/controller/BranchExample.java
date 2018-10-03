0c24bcd7f49fc415fe7e1f4e232cfcf753c2be24
SUSHANT <SUSHANT@SUSHANT>
2018-10-03 17:48:44 +0530

    Creating a brance2

---
+++ springmvc/src/com/app/controller/BranchExample.java
@@ -0,0 +1,21 @@
+package com.app.controller;
+
+public class BranchExample {
+	//I am create a brance2 for adding new functionality 
+	private int sno;
+	private String sname;
+	public int getSno() {
+		return sno;
+	}
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	
+
+}
